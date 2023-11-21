e1ac688d429ce4c3f0b26745bcfe3e23b2793a0a
Admin <Admin@DESKTOP-H6VA1KR>
2023-11-21 11:35:11 +0530

    aaa

---
+++ Jenkinsproject/src/test/java/testcases/TC5test.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC5test {
+	@Test
+	public void m1()
+	{
+		Reporter.log("true",true);
+	}
+
+}
